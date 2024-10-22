93e7d833f06098d02e915c107e138b4c7802df78
Alessia Vanni <alevanni19@gmail.com>
2024-10-22 15:53:32 +0200

    Basic structure
    Defined the very basic file structure.

---
+++ src/components/ScoreTable.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+// component that displays the score tables
+
+import { ref } from 'vue'
+
+
+
+</script>
+
+<template>
+    <div class="score-table">
+
+    </div>
+</template>
---
+++ src/components/ThrownDice.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+
+// component that displays thrown dice and 
+// contains the button to throw them
+
+import { ref } from 'vue'
+
+
+
+</script>
+
+<template>
+    <div class="dice-board">
+        <table id="dice">
+                <tr>
+                    <td class="die">&#x2680;</td>
+                    <td class="die">&#x2681;</td>
+                    <td class="die">&#x2682;</td>
+                    <td class="die">&#x2683;</td>
+                    <td class="die">&#x2684;</td>
+                </tr>
+            </table>
+            <!-- button that throws the dice -->
+            <button id="gooi">Throw!</button>
+    </div>
+</template>
